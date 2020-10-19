d8d760569151fd65ae4d6798cbd79cd2b108cb25
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2020-10-19 20:37:02 +0530

    Create prb.c

---
+++ problem-8/prb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,value,sum=0;
+    scanf("%d", &n);  #taking user input
+    for(i=1;i<=10;i++)
+    {
+        value=n*i;  #computing the table values from 1 to 10
+        printf("%d ",value);  #printing table values of given number separated by space
+        sum=sum+value;  #calculating the sum of all table values
+    }
+    printf("\n%d",sum);  #displaying the sum
+    return 0;
+}
