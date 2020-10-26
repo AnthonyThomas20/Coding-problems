2058bbb4c16e28e88654d728d8cd415a32342951
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2020-10-26 19:01:21 +0530

    Update prb10.c

--- problem-10/prb10.c
+++ problem-10/prb10.c
@@ -7,22 +7,22 @@ int sumOfValue(int len, int arr[], int value)
     {
         if(arr[i]%value==0)
         {
-            sum=sum+arr[i];    #adding all elements of the array that are divisible by the input value
+            sum=sum+arr[i];    //adding all elements of the array that are divisible by the input value
         }
     }
-    printf("%d",sum);    #displaying the sum
+    printf("%d",sum);    //displaying the sum
 }
 
 int main()
 {
     int i,len,value;
-    scanf("%d", &len);    #taking array length from user
+    scanf("%d", &len);    //taking array length from user
     int arr[len];
     for(i=0;i<len;i++)
     {
-        scanf("%d",&arr[i]);    #taking array elements from user
+        scanf("%d",&arr[i]);    //taking array elements from user
     }
-    scanf("%d", &value);    #taking required value from user
+    scanf("%d", &value);    //taking required value from user
     sumOfValue(len,arr,value);    
     return 0;
 }
