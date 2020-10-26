6cd8f4e38f8a703b3c493e7ddb97f12c3471aed2
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2020-10-26 19:00:17 +0530

    Update prb.c

--- problem-8/prb.c
+++ problem-8/prb.c
@@ -3,13 +3,13 @@
 int main()
 {
     int n,i,value,sum=0;
-    scanf("%d", &n);  #taking user input
+    scanf("%d", &n);  //taking user input
     for(i=1;i<=10;i++)
     {
-        value=n*i;  #computing the table values from 1 to 10
-        printf("%d ",value);  #printing table values of given number separated by space
-        sum=sum+value;  #calculating the sum of all table values
+        value=n*i;  //computing the table values from 1 to 10
+        printf("%d ",value);  //printing table values of given number separated by space
+        sum=sum+value;  //calculating the sum of all table values
     }
-    printf("\n%d",sum);  #displaying the sum
+    printf("\n%d",sum);  //displaying the sum
     return 0;
 }
