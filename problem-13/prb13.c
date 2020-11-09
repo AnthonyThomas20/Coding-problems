a9206edd4a6c3ca69b0d7eeb058cc45ca450ed2c
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2020-11-09 18:33:25 +0530

    Update prb13.c

--- problem-13/prb13.c
+++ problem-13/prb13.c
@@ -4,24 +4,31 @@ int main()
 {
     int n,i,a=0,b=0;
     scanf("%d", &n);
-    for(i=1;i<=n;i++)
+    if(n<1)
     {
-        if(i%2!=0)
-        {
-            a=a+7;
-        }
-        else
-        {
-            b=b+6;
-        }
-    }
-    if(n%2!=0)
-    {
-        printf("%d", a-7);
+        printf("\nINVALID INPUT");
     }
     else
     {
-        printf("%d", b-6);
+        for(i=1;i<=n;i++)
+        {
+            if(i%2!=0)
+            {
+                a=a+7;
+            }
+            else
+            {
+                b=b+6;
+            }
+        }
+        if(n%2!=0)
+        {
+            printf("\n%d", a-7);
+        }
+        else
+        {
+            printf("\n%d", b-6);
+        }
     }
     return 0;
 }
