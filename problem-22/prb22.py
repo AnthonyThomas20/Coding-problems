ecfbeeab78cdb02592a52185f291555cb1197a06
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2020-11-10 10:46:18 +0530

    Update prb22.py

--- problem-22/prb22.py
+++ problem-22/prb22.py
@@ -1,7 +1,11 @@
 a = list(str(input()))
 b = list(reversed(a))
 count = 0
-for i in range(len(a)):
-    if(a[i] == b[i]):
-        count = count + 1
-print("\n" + str(count))
+
+if(len(a) == 0 or len(a)>100):
+    print("INVALID INPUT")
+else:
+    for i in range(len(a)):
+        if(a[i] == b[i]):
+            count = count + 1
+    print("\n" + str(count))
