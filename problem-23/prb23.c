bb67340c46a859932ffbcb11556dc17224de78cd
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2020-11-12 16:17:00 +0530

    Update prb23.c

--- problem-23/prb23.c
+++ problem-23/prb23.c
@@ -4,15 +4,22 @@ int main()
 {
     int n,i,count=0;
     scanf("%d",&n);
-    int a[n];
-    for(i=0;i<n;i++)
+    if(n<0 || n>1000000)
     {
-        scanf("%d",&a[i]);
-        if(a[i]<0)
-        {
-            count++;
-        }
+        printf("\nINVALID INPUT");
     }
-    printf("\n%d",count);
+    else
+    {
+        int a[n];
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+            if(a[i]<0)
+            {
+                count++;
+            }
+        }
+        printf("\n%d",count);
+    }
     return 0;
 }
