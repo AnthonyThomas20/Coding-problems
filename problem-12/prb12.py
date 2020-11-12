60ad0afd3b5896366cdbffbb94352dbc78e6cc82
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2020-11-12 15:12:56 +0530

    Update prb12.py

--- problem-12/prb12.py
+++ problem-12/prb12.py
@@ -1,6 +1,7 @@
 def add(a, b):
-    sum = float(a) + b
-    print("Sum = " + str(sum))
+    s = float(a) + b
+    result = round(s,2)
+    print("Sum = " + str(result))
 
 n1 = int(input("Number 1 - "))
 n2 = float(input("Number 2 - "))
