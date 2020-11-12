638db6ad043af007ec817e9ab8660a4f8e8af092
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2020-11-12 15:04:02 +0530

    Update prb11.py

--- problem-11/prb11.py
+++ problem-11/prb11.py
@@ -1,8 +1,7 @@
-n = 10
 x = int(input())
 if(x > 1 and x <= 5):
     print("\nNUMBER OF CANDIES SOLD : " + str(x))
-    print("NUMBER OF CANDIES AVAILABLE : " + str(n-x))
+    print("NUMBER OF CANDIES AVAILABLE : " + str(10-x))
 else:
     print("\nINVALID INPUT : " + str(x))
-    print("NUMBER OF CANDIES LEFT : " + str(n-x))
+    print("NUMBER OF CANDIES LEFT : " + str(10))
