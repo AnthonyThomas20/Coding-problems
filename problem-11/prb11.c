014232624724bc4aa3cc0b270e03ba9897121989
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2020-11-12 15:02:32 +0530

    Update prb11.c

--- problem-11/prb11.c
+++ problem-11/prb11.c
@@ -2,20 +2,21 @@
 
 int main()
 {
-    int n=10,x;
-    //n is the capacity of jar
-    //k = 5 is the minimum number of candies that should remain in the jar
-    //x is the user input
+    int x;
     scanf("%d", &x);
+    //The capacity of jar is 10
+    //The minimum number of candies that should remain in the jar is 5
+    //x is the user input
+    
     if(x>1 && x<=5)
     {
         printf("\nNUMBER OF CANDIES SOLD : %d", x);
-        printf("\nNUMBER OF CANDIES AVAILABLE : %d", n-x);
+        printf("\nNUMBER OF CANDIES AVAILABLE : %d", 10-x);
     }
     else
     {
         printf("\nINVALID INPUT");
-        printf("\nNUMBER OF CANDIES LEFT : %d", n);
+        printf("\nNUMBER OF CANDIES LEFT : 10");
     }
     return 0;
 }
