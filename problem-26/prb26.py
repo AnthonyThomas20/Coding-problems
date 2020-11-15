5f18c197ad337217251c5e241de63d6428458ed4
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2020-11-15 22:27:58 +0530

    Update prb26.py

--- problem-26/prb26.py
+++ problem-26/prb26.py
@@ -1,10 +1,13 @@
+def countpage(n,p):
+    t = 0
+    t = (n/2 - p/2)/2
+    if(t < p/2):
+        t = p/2
+    print("\n" + str(int(t)))
+
 n = int(input())
 p = int(input())
-t = 0
 # n = number of pages in the book
 # p = required page number
 # t = number of pages to be turned to reach pth page
-t = (n/2 - p/2)/2
-if(t < p/2):
-    t = p/2
-print("\n" + str(int(t)))
+countpage(n,p)
