31663bc0bd9350dccdd9eaf1bbc2ad22075a203f
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2020-11-15 22:25:52 +0530

    Update prb26.c

--- problem-26/prb26.c
+++ problem-26/prb26.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
-int main()
+
+int countpage(int n, int p)
 {
-    int n,p,t;
-    scanf("%d",&n);
-    scanf("%d",&p);
-    // n = number of pages in the book
-    // p = required page number
-    // t = number of pages to be turned to reach pth page
-    
+    int t;
     t = (n/2 - p/2)/2;
     if(t<p/2)
     {
         t=p/2;
     }
     printf("\n%d",t);
+}
+ 
+int main()
+{
+    int n,p;
+    scanf("%d",&n);
+    scanf("%d",&p);
+    // n = number of pages in the book
+    // p = required page number
+    // t = number of pages to be turned to reach pth page
+    countpage(n,p);
     return 0;
 }
