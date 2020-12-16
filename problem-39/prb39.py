0c2944da49c7a6d1912751fc5e37175503fea6b9
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2020-12-16 06:51:47 +0530

    Create prb39.py

---
+++ problem-39/prb39.py
@@ -0,0 +1,11 @@
+n = int(input())
+if(n >= 30 and n <= 50):
+    print("D")
+elif(n >= 51 and n <= 60):
+    print("C")
+elif(n >= 61 and n <= 80):
+    print("B")
+elif(n >= 81 and n <= 100):
+    print("A")
+else:
+    print("Invalid Input")
